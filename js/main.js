acf5542e5e92cf5155996142b22f82ec5221c302
Manthan Bhatt <manthanbhatt6225@gmail.com>
2020-10-14 18:10:01 +0530

    2020-10-14-manthan-version one for website

---
+++ js/main.js
@@ -0,0 +1,169 @@
+$(window).scroll(function () {
+    if ($(document).scrollTop() > 50) {
+        $('nav').addClass('transparent');
+        $('.navbar').addClass('navbar-light');
+        $('.navbar').removeClass('navbar-dark');
+    } else {
+        $('nav').removeClass('transparent');
+        $('.navbar').addClass('navbar-dark');
+        $('.navbar').removeClass('navbar-light');
+    }
+});
+
+if ($(document).scrollTop() > 50) {
+    $('nav').addClass('transparent');
+    $('.navbar').addClass('navbar-light');
+    $('.navbar').removeClass('navbar-dark');
+} else {
+    $('nav').removeClass('transparent');
+    $('.navbar').addClass('navbar-dark');
+    $('.navbar').removeClass('navbar-light');
+}
+
+localStorage.setItem('version', '1');
+
+let bookNow = () => {
+    window.location.href = "https://live.ipms247.com/booking/book-rooms-thesentosavilla";
+}
+
+let goToRoom = () => {
+    window.location.href = "room.html";
+}
+
+let openImage = (src) => {
+    $.magnificPopup.open({
+        items: {
+            src: src
+        },
+        type: 'image'
+    });
+};
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+
+let saveEnquiry = () => {
+    let name = $('#name').val();
+    let email = $('#email').val();
+    let mobileNumber = $('#mobileNumber').val();
+    let guest = $('#guest').val();
+    let accommodation = $('#accommodation').val();
+    let message = $('#message').val();
+
+    if (!name) {
+        $('#name').popover({
+            trigger: 'focus',
+            content: 'Please enter your name',
+        });
+        $("#name").focus();
+        return;
+    } else if (!email) {
+        $('#email').popover({
+            trigger: 'focus',
+            content: 'Please enter your email',
+        });
+        $("#email").focus();
+        return;
+    } else if (!mobileNumber) {
+        $('#mobileNumber').popover({
+            trigger: 'focus',
+            content: 'Please enter your mobile number',
+        });
+        $("#mobileNumber").focus();
+        return;
+    } else if (mobileNumber.length != 10) {
+        $('#mobileNumber').popover({
+            trigger: 'focus',
+            content: 'Please enter valid mobile number',
+        });
+        $("#mobileNumber").focus();
+        return;
+    } else if (!guest) {
+        $('#guest').popover({
+            trigger: 'focus',
+            content: 'Please select the number of guests',
+        });
+        $("#guest").focus();
+        return;
+    } else if (!accommodation) {
+        $('#accommodation').popover({
+            trigger: 'focus',
+            content: 'Please select the accommodation type',
+        });
+        $("#accommodation").focus();
+        return;
+    } else if (!message) {
+        $('#message').popover({
+            trigger: 'focus',
+            content: 'Please enter your message',
+        });
+        $("#message").focus();
+        return;
+    }
+
+    $('#booking-btn-text').hide();
+    $('#booking-btn').prop('disabled',true);
+    $('.spinner-grow').show();
+    $.ajax({
+        type: "POST",
+        url: "http://localhost/sentosa_villa/service/mail-service.php",
+        data: JSON.stringify({
+            name,
+            email,
+            mobileNumber,
+            guest,
+            accommodation,
+            message,
+            type: 'booking'
+        }),
+        success: (resp) => {
+            console.log('second success');
+            console.log(resp);
+            $('.booking-form').hide();
+            $('#booking-success').show(300);
+            $('#booking-btn-text').show();
+            $('#booking-btn').prop('disabled',false);
+            $('.spinner-grow').hide();
+        },
+        error: (err) => {
+            console.log('error');
+            console.log(err);
+            $('#booking-btn-text').show();
+            $('#booking-btn').prop('disabled',false);
+            $('.spinner-grow').hide();
+        }
+    });
+}
+
+$('#booking-success').hide();
+$('.spinner-grow').hide();
+
+
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        setTimeout(() => {
+            document.querySelector("#loader").style.display = "none";
+            document.querySelector("body").style.visibility = "visible";
+        }, 500);
+    }
+};
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
---
+++ service/mail-service.php
@@ -0,0 +1,38 @@
+<?php
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    exit(0);
+}
+
+date_default_timezone_set("Asia/kolkata");
+
+$data = file_get_contents('php://input');
+$request = json_decode($data);
+
+if (!isset($request->type)) {
+    echo json_encode(["status" => 500, "message" => "Internal Error"]);
+}
+
+$to = "info@thesentosavilla.com";
+$subject = $request->type == "booking" ? "Booking Enquiry " . date("Y-m-d H:i:s") . " From " . $request->name : $request->type;
+
+if ($request->type == "booking") {
+    $txt = "Booking Enquiry from " . $request->name . " below are the details \n Name: " . $request->name . " \n Email: " . $request->email . " \n Mobile Number: " . $request->mobileNumber . " \n Guest: " . $request->guest . " \n Accommodation Type: " . $request->accommodation . " \n Message: " . $request->message;
+} else {
+    $txt = "Booking Enquiry from " . $request->name . " below are the details \n Name: " . $request->name . " \n Email: " . $request->email . " \n Mobile Number: " . $request->mobileNumber . " \n Guest: " . $request->guest . " \n Accommodation Type: " . $request->accommodation . " \n Message: " . $request->message;
+}
+
+try {
+    mail($to, $subject, $txt);
+
+    echo json_encode(["status" => 200, "message" => "Thank you for enquiry, Our executive will call you shortly."]);
+} catch (Exception $e) {
+    echo json_encode(["status" => 500, "message" => "Internal Error", "exception" => $e]);
+}
